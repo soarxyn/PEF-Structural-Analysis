10cd4c516c6bb1ec905c0f2a7524d85c0966b3aa
Aimnos <roikegami@hotmail.com>
2020-04-29 14:10:28 -0300

    Added vector to represent position

--- reforcos.py
+++ reforcos.py
@@ -12,19 +12,24 @@ class TipoReforco(Enum):
   ENGASTE : tuple = (3, 2, 1)
 
 class Reforco:
-  def __init__(self, nome):
+  def __init__(self, nome, x, y):
     self.tipo : Enum = TipoReforco[nome]
     self.nome : str = self.tipo.name
+    self.posicao : Vec3d = Vec3d(x, y, None)
     self.forcas : int = 2 if self.tipo.value[1] > 1 else 1
     self.momentos : int = 1 if self.tipo.value[2] == 1 else 0
     self.reacao : Vec3d = Vec3d(0 if self.forcas == 2 else None, 0, 0 if self.momentos == 1 else None)
 
 if __name__ == "__main__":
   nome : str = input('Dê tipo do reforço: ')
-  apoio : Reforco = Reforco(nome)
-  print(f'tipo: {apoio.tipo}')
-  print(f'numero de forcas: {apoio.forcas}')
-  print(f'numero de momentos: {apoio.momentos}')
-  print(f'modulo da forca em x: {apoio.reacao.x}')
-  print(f'modulo da forca em y: {apoio.reacao.y}')
-  print(f'modulo do momento em z: {apoio.reacao.z}')
+  x : float = input('Dê a coordenada x do reforço: ')
+  y : float = input('Dê a coordenada y do reforço: ')
+  reforco : Reforco = Reforco(nome, x, y)
+  print(f'tipo: {reforco.tipo}')
+  print(f'numero de forcas: {reforco.forcas}')
+  print(f'numero de momentos: {reforco.momentos}')
+  print(f'coordenada x do reforco: {reforco.posicao.x}')
+  print(f'coordenada y do reforco: {reforco.posicao.y}')
+  print(f'modulo da forca em x: {reforco.reacao.x}')
+  print(f'modulo da forca em y: {reforco.reacao.y}')
+  print(f'modulo do momento em z: {reforco.reacao.z}')
