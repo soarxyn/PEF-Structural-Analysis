e02f4d8ee91c29630c3b00fad7c3be1f2f7d10cf
Aimnos <roikegami@hotmail.com>
2020-04-29 21:00:49 -0300

    Removed unnecessary parameter from enum tuples

--- reforcos.py
+++ reforcos.py
@@ -7,17 +7,17 @@ class Vec3d:
     self.z : float = z
 
 class TipoReforco(Enum):
-  APOIO : tuple = (1, 1, 0)
-  ARTICULACAO : tuple = (2, 2, 0)
-  ENGASTE : tuple = (3, 2, 1)
+  APOIO : tuple = (1, 0)
+  ARTICULACAO : tuple = (2, 0)
+  ENGASTE : tuple = (2, 1)
 
 class Reforco:
   def __init__(self, nome, x, y):
     self.tipo : Enum = TipoReforco[nome]
     self.nome : str = self.tipo.name
     self.posicao : Vec3d = Vec3d(x, y, None)
-    self.forcas : int = 2 if self.tipo.value[1] > 1 else 1
-    self.momentos : int = 1 if self.tipo.value[2] == 1 else 0
+    self.forcas : int = 2 if self.tipo.value[0] > 1 else 1
+    self.momentos : int = 1 if self.tipo.value[1] == 1 else 0
     self.reacao : Vec3d = Vec3d(0 if self.forcas == 2 else None, 0, 0 if self.momentos == 1 else None)
 
 if __name__ == "__main__":
