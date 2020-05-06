582c0373441d246ba2b8145b3ba050682bc0fb3c
Lamino1788 <62446363+Lamino1788@users.noreply.github.com>
2020-05-06 13:42:26 -0300

    Update Barra PEF.py

--- Barra PEF.py
+++ Barra PEF.py
@@ -1,9 +1,4 @@
-class Vec3d:
-  def __init__(self, x, y, z):
-    self.x : float = x
-    self.y : float = y
-    self.z : float = z
-
+from auxiliary.vectors import Vec3d
 
 class Barra(object):
     forcas : Vec3d = []
