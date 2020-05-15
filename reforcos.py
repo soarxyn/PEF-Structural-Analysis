27c305a2aec91a876cc794a778b554c782776898
Aimnos <roikegami@hotmail.com>
2020-05-15 13:36:50 -0300

    Removed position vector

--- reforcos.py
+++ reforcos.py
@@ -7,10 +7,9 @@ class TipoReforco(enum.Enum):
   ENGASTE : tuple = (2, 1)
 
 class Reforco:
-  def __init__(self, nome, x, y):
+  def __init__(self, nome):
     self.tipo : enum = TipoReforco[nome]
     self.nome : str = self.tipo.name
-    self.posicao : Vector3 = Vector3(x, y, None)
     self.forcas : int = 2 if self.tipo.value[0] > 1 else 1
     self.momentos : int = 1 if self.tipo.value[1] == 1 else 0
     self.reacao : Vector3 = Vector3(0 if self.forcas == 2 else None, 0, 0 if self.momentos == 1 else None)
