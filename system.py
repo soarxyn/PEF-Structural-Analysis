1cdf11a9d57a22b8fcec1d745b654e52df4ce42e
Aimnos <roikegami@hotmail.com>
2020-06-04 13:28:25 -0300

    Updated comments

--- system.py
+++ system.py
@@ -1,4 +1,3 @@
-import math
 from typing import List, Tuple
 from auxiliary.algebra import Vector3, Polynomial, Matrix3x3, solve
 from beam import Beam
@@ -6,38 +5,38 @@ from force import Concentrated, Distributed, Moment
 from support import Support
 
 class System:
-  beams: List[Tuple[Beam, Vector3, float, Vector3]] = [] # the tuple vector is the beam's position, with respect to the center of the
-                                                # coordinate system, while the float is its angle with respect to the x axis
+  beams: List[Tuple[Beam, Vector3, float, Vector3]] = list()  # the tuple vectors are the beam's start and end position, respectively, with respect to the
+                                                              # center of the coordinate system, while the float is its angle with respect to the x axis
 
   def solveSystem(self):
     coefs: Matrix3x3 = Matrix3x3([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
     b: Vector3 = Vector3(0, 0, 0)
 
-    h: List[Tuple[Vector3, Vector3]] = [] # the first vector is the reaction force from the support and the second
-    v: List[Tuple[Vector3, Vector3]] = [] # one is its position with respect to the center of the coordinate system
-    m: List[Vector3] = [] # the vector is the reaction force from the support
+    h: List[Tuple[Vector3, Vector3]] = list() # the first vector is the reaction force from the support and the second
+    v: List[Tuple[Vector3, Vector3]] = list() # one is its position with respect to the center of the coordinate system
+    m: List[Vector3] = list() # the vector is the reaction force from the support
 
     result: Vector3 = None
-    r: List[float] = []
+    r: List[float] = list()
 
     for beam in self.beams:
       if isinstance(beam[0].start, Support):
         reaction: Vector3 = beam[0].start.reaction
         pos: Vector3 = beam[1]
-        v.append([reaction, pos])
+        v.append(tuple(reaction, pos))
         if reaction.x != None:
-          h.append([reaction, pos])
+          h.append(tuple(reaction, pos))
         if reaction.z != None:
-          m.append([reaction, pos])
+          m.append(tuple(reaction, pos))
 
       if isinstance(beam[0].end, Support):
         reaction: Vector3 = beam[0].end.reaction
         pos: Vector3 = beam[0].pointPos(beam[1], beam[0].length, beam[2])
-        v.append([reaction, pos])
+        v.append(tuple(reaction, pos))
         if reaction.x != None:
-          h.append([reaction, pos])
+          h.append(tuple(reaction, pos))
         if reaction.z != None:
-          m.append([reaction, pos])
+          m.append(tuple(reaction, pos))
 
 
       for concentrated in beam[0].concentratedList:
@@ -55,8 +54,7 @@ class System:
         b.y -= force.y
         b.z -= force.y*pos.x - force.x*pos.y
 
-      for moment in beam[0].momentList:
-        b.z -= moment.magnitude
+      b.z -= beam[0].moment.magnitude
 
 
       if len(h) + len(v) + len(m) == 3:
