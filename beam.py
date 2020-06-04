54adcce0dba0f4330bf06c92ab20686c3abc6115
Aimnos <roikegami@hotmail.com>
2020-06-04 13:26:53 -0300

    Now using custom trig functions

--- beam.py
+++ beam.py
@@ -1,55 +1,23 @@
 from __future__ import annotations
-from typing import List, Tuple, Union
-import math
-from auxiliary.algebra import Vector3
+from typing import List, Tuple
+from auxiliary.algebra import Vector3, psin, pcos
 from force import Concentrated, Distributed, Moment
 from support import Support
 
 class Beam:
 	length: float = None
-	start: Union[Beam, Support] = None	# beam can be attached to other
-	end: Union[Beam, Support] = None		# beams or to a support
+	start: Tuple[Support, List[Beam]] = tuple(None, list())	# beam can be attached to other
+	end: Tuple[Support, List[Beam]] = tuple(None, list())		# beams or to a support
 
-	concentratedList: List[Tuple[Concentrated, float, float]] = []	# tuple floats are the relative
-	distributedList: List[Tuple[Distributed, float, float]] = []		# position and angle, in that order
-	momentList: List[Moment] = []
+	concentratedList: List[Tuple[Concentrated, float, float]] = list()	# tuple floats are the relative
+	distributedList: List[Tuple[Distributed, float, float]] = list()		# position and angle, in that order
+	moment: Moment = None
 
 	def __init__(self, length: float):
 		self.length = length
 
 	def pointPos(self, startPos: Vector3, point: float, angle: float) -> Vector3:
-		return startPos + Vector3(point*math.cos(angle), point*math.sin(angle), 0)
+		if point > self.length or point < 0:
+			raise Exception('Point is outside the beam')
 
-	def leftCut(self, point: float):
-		left: Beam = Beam(point)
-
-		for concentrated in self.concentratedList:
-			if concentrated[1] < point:
-				left.concentratedList.append(concentrated)
-
-		for distributed in self.distributedList:
-			if distributed[1] < point:
-				left.distributedList.append(distributed)
-
-		for moment in self.momentList:
-			if moment[1] < point:
-				left.momentList.append(moment)
-
-		return left
-
-	def rightCut(self, point: float):
-		right: Beam = Beam(point)
-
-		for concentrated in self.concentratedList:
-			if concentrated[1] >= point:
-				right.concentratedList.append(concentrated)
-
-		for distributed in self.distributedList:
-			if distributed[1] >= point:
-				right.distributedList.append(distributed)
-
-		for moment in self.momentList:
-			if moment[1] >= point:
-				right.momentList.append(moment)
-
-		return right
+		return startPos + Vector3(point*pcos(angle), point*psin(angle), 0)
--- force.py
+++ force.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
-import math
 from typing import Tuple
-from auxiliary.algebra import Vector3, Polynomial, integrate
+from auxiliary.algebra import Vector3, Polynomial, integrate, psin, pcos, pcot
 
 class Concentrated:
 	magnitude: float = None
@@ -11,8 +10,8 @@ class Concentrated:
 
 	def forceVector(self, angle: float) -> Vector3:
 		return Vector3(
-			-self.magnitude*math.cos(angle),
-			-self.magnitude*math.sin(angle),
+			-self.magnitude*pcos(angle),
+			-self.magnitude*psin(angle),
 			0
 		)
 
@@ -24,27 +23,27 @@ class Distributed:
 		self.length = length
 		self.distribution = distribution
 
-	def equivalent(self) -> Tuple[Concentrated, float]:				# tuple float is the equivalent force's point of application
+	def equivalent(self) -> Tuple[Concentrated, float]:	# tuple float is the equivalent force's point of application
 		p1: Polynomial = Polynomial(self.distribution.coefficients)
 		p1.coefficients.insert(0, 0)
 		p1.degree = self.distribution.degree + 1
 		integral: float = integrate(self.distribution, 0, self.length)
-		return [
+		return tuple(
 			Concentrated(integral),
 			integrate(p1, 0, self.length)/integral
-		]
+		)
 
 	def angledComponents(self, angle: float) -> Tuple[Distributed, Distributed]:	# the first load is applied perpendicularly to the referential axis, while the second one is applied in parallel
 		basePolynomial: Polynomial = Polynomial(self.distribution.coefficients)
 		if self.distribution.degree < 1:
-			basePolynomial.coefficients.append(-1/math.tan(angle))
+			basePolynomial.coefficients.append(-pcot(angle))
 			basePolynomial.degree = 1
 		else:
-			basePolynomial.coefficients[1] -= 1/math.tan(angle)
-		return [
-			Distributed(self.length/math.sin(angle), math.sin(angle)*basePolynomial),
-			Distributed(self.length/math.sin(angle), -math.cos(angle)*basePolynomial)
-		]
+			basePolynomial.coefficients[1] -= pcot(angle)
+		return tuple(
+			Distributed(self.length/psin(angle), psin(angle)*basePolynomial),
+			Distributed(self.length/psin(angle), -pcos(angle)*basePolynomial)
+		)
 
 class Moment:
 	magnitude: float = None
