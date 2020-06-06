65b2fad92b4b9fcc087098e4e2c0f6a50842cc5d
Aimnos <roikegami@hotmail.com>
2020-06-06 16:38:47 -0300

    First complete solveSystem implementation

--- system.py
+++ system.py
@@ -1,6 +1,6 @@
-from typing import List, Tuple
-from auxiliary.algebra import Vector3, Polynomial, Matrix3x3, solve
-from beam import Beam
+from typing import List, Tuple, Callable, Union
+from auxiliary.algebra import Vector3, Polynomial, Matrix3x3, solve, rotate
+from beam import Beam, BoundedStressFunctions
 from force import Concentrated, Distributed, Moment
 from support import Support
 
@@ -9,7 +9,7 @@ class System:
     self.beams: List[Tuple[Beam, Vector3, float, Vector3]] = list() # the tuple vectors are the beam's start and end position, respectively, with respect to the
                                                                     # center of the coordinate system, while the float is its angle with respect to the x axis
 
-  def solveSystem(self):
+  def solveSystem(self) -> List[Tuple[List[BoundedStressFunctions], bool]]:
     coefs: Matrix3x3 = Matrix3x3([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
     b: Vector3 = Vector3(0, 0, 0)
 
@@ -18,16 +18,20 @@ class System:
     result: Vector3 = None
     r: List[float] = list()
 
+    DFSRoot: Beam
+
     def scaleBeam(b):
       return (b[0], Vector3(b[1].x, -b[1].y, b[1].z)*0.1, b[2], Vector3(b[3].x, -b[3].y, b[3].z)*0.1)
 
     scaledBeams = list(map(lambda b: scaleBeam(b), self.beams.copy()))
     for beam in scaledBeams:
-      if isinstance(beam[0].start[0], Support):
+      if beam[0].start[0] != None:
         supports.append((beam[0].start[0].reaction, beam[1]))
+        DFSRoot = beam[0]
 
-      if isinstance(beam[0].end[0], Support):
+      if beam[0].end[0] != None:
         supports.append((beam[0].end[0].reaction, beam[3]))
+        DFSRoot = beam[0]
 
 
       for concentrated in beam[0].concentratedList:
@@ -48,42 +52,89 @@ class System:
       if beam[0].moment != None:
         b.z -= beam[0].moment.magnitude
 
-      i: int = 0
+    i: int = 0
+    for s in supports:
+      if s[0].x != 0:
+        coefs[0][i] = s[0].x
+        coefs[2][i] = -s[1].y
+        i += 1
+
+      if s[0].y != 0:
+        coefs[1][i] = s[0].y
+        coefs[2][i] = s[1].x
+        i += 1
+
+      if s[0].z != 0:
+        coefs[2][i] = s[0].z
+        i += 1
+
+
+    if i == 3:
+      result = solve(coefs, b)
+      r = [result.x, result.y, result.z]
+
+      i = 0
       for s in supports:
         if s[0].x != 0:
-          coefs[0][i] = s[0].x
-          coefs[2][i] = -s[1].y
+          s[0].x = r[i]
           i += 1
 
         if s[0].y != 0:
-          coefs[1][i] = s[0].y
-          coefs[2][i] = s[1].x
+          s[0].y = r[i]
           i += 1
 
         if s[0].z != 0:
-          coefs[2][i] = s[0].z
+          s[0].z = r[i]
           i += 1
 
-
-      if i == 3:
-        result = solve(coefs, b)
-        r = [result.x, result.y, result.z]
-
-        i = 0
-        for s in supports:
-          if s[0].x != 0:
-            s[0].x = r[i]
-            i += 1
-
-          if s[0].y != 0:
-            s[0].y = r[i]
-            i += 1
-
-          if s[0].z != 0:
-            s[0].z = r[i]
-            i += 1
+    else:
+      raise Exception('System is not isostatic!')
 
 
+    solution: List[Tuple[List[BoundedStressFunctions], bool]] = list()
+
+    def solveBeamsDFS(b: Beam, p: Union[Beam, None]) -> Tuple[Vector3, float]:
+      v: Vector3 = Vector3(0, 0 ,0)
+      endFirst: bool
+      s: Tuple[Vector3, float]
+      i: int
+
+      for i in range(len(self.beams)):
+        if b == self.beams[i][0]:
+          break
+
+      if b.start[0] != None:
+        v = rotate(b.start[0].reaction, self.beams[i][2])
+        endFirst = False
+      elif b.end[0] != None:
+        v = rotate(b.end[0].reaction, self.beams[i][2])
+        endFirst = True
+
+      elif p != None:
+        if p in b.start[1]:
+          for beam in b.end[1]:
+            s = solveBeamsDFS(beam, b)
+            v += rotate(s[0], self.beams[i][2] - s[1])
+
+          endFirst = True
+
+        elif p in b.end[1]:
+          for beam in b.start[1]:
+            s = solveBeamsDFS(beam, b)
+            v += rotate(s[0], self.beams[i][2] - s[1])
+
+          endFirst = False
+
+        else:
+          raise Exception('Cannot find reaction!')
       else:
-        raise Exception('System is not isostatic!')
-      
+        raise Exception('No parent given!')
+
+      solution[i] = (beam[0].solve(v, endFirst), endFirst)
+
+      return (v, self.beams[i][2])
+
+
+    solveBeamsDFS(DFSRoot, None)
+
+    return solution
