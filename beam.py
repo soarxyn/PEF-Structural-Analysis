854465df3265e8edebc8c93a6d8ac162bc7d170b
Aimnos <roikegami@hotmail.com>
2020-06-08 21:09:03 -0300

    solve functions fix

--- beam.py
+++ beam.py
@@ -39,7 +39,8 @@ class Beam:
 			resulting.z -= resulting.y*abs(pos - prev)
 			v: Vector3
 			if isinstance(force[0], Distributed):
-				pos -= force[0].length if endFirst else -force[0].length
+				prev = pos
+				pos += force[0].length
 				a: Tuple[Distributed, Distributed] = force[0].angledComponents(force[2])
 				a[0].distribution.coefficients[0] -= resulting.x
 				a[1].distribution.coefficients[0] += resulting.y
@@ -65,15 +66,12 @@ class Beam:
 			for i in range(len(stress)):
 				if x < stress[i][1]:
 					p: float = stress[i - 1][1] if i > 0 else 0
-					d = i + d
-					if d < len(forces):
-						if isinstance(forces[d][0], Distributed):
-							return integrate(stress[i][0][0], abs(x - p), forces[d][0].length) if endFirst else integrate(stress[i][0][0], 0, abs(x - p))
-
+					if stress[i][0][2].degree == 0:
+						return integrate(stress[i][0][0], abs(x - p), forces[i - d][0].length) if endFirst else integrate(stress[i][0][0], 0, abs(x - p))
 					else:
 						return stress[i][0][0](abs(x - p))
 
-				if isinstance(forces[i + d][0], Distributed):
+				if stress[i][0][2].degree == 0:
 					d += 1
 
 			return -resulting.x
@@ -83,15 +81,12 @@ class Beam:
 			for i in range(len(stress)):
 				if x < stress[i][1]:
 					p: float = stress[i - 1][1] if i > 0 else 0
-					d = i + d
-					if d < len(forces):
-						if isinstance(forces[d][0], Distributed):
-							return integrate(stress[i][0][1], abs(x - p), forces[d][0].length) if endFirst else integrate(stress[i][0][1], 0, abs(x - p))
-
+					if stress[i][0][2].degree == 0:
+						return integrate(stress[i][0][1], abs(x - p), forces[i - d][0].length) if endFirst else integrate(stress[i][0][1], 0, abs(x - p))
 					else:
 						return stress[i][0][1](abs(x - p))
 
-				if isinstance(forces[i + d][0], Distributed):
+				if stress[i][0][2].degree == 0:
 					d += 1
 
 			return resulting.y
@@ -101,18 +96,15 @@ class Beam:
 			for i in range(len(stress)):
 				if x < stress[i][1]:
 					p: float = stress[i - 1][1] if i > 0 else 0
-					d = i + d
-					if d < len(forces):
-						if isinstance(forces[d][0], Distributed):
-							e: Tuple[Concentrated, float] = forces[d][0].equivalent(abs(x - p), forces[d][0].length) if endFirst else forces[d][0].equivalent(0, abs(x - p))
-							return stress[i][0][2](abs(x - p)) + e[0].y*e[1] if endFirst else stress[i][0][2][0] + e[0].y*abs(x - p - e[1])
-
+					if stress[i][0][2].degree == 0:
+						e: Tuple[Concentrated, float] = forces[i - d][0].equivalent(abs(x - p), forces[i - d][0].length) if endFirst else forces[i - d][0].equivalent(0, abs(x - p))
+						return stress[i][0][2](abs(x - p)) + e[0].y*e[1] if endFirst else stress[i][0][2][0] + e[0].y*abs(x - p - e[1])
 					else:
 						return stress[i][0][2](abs(x - p))
 
-				if isinstance(forces[i + d][0], Distributed):
+				if stress[i][0][2].degree == 0:
 					d += 1
 
-			return resulting.y*(abs(x - pos)) - resulting.z
+			return resulting.y*abs(x - pos) - resulting.z
 
 		return (normal, shear, bending)
